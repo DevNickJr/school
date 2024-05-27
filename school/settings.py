3da964feb56a6580a4141375bb9bdd59e735e283
DevNickjr <nicholasduadei14@gmail.com>
2024-05-27 14:18:07 +0100

    display data

--- school/settings.py
+++ school/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ikjo(z**)*7ux2o79ogucu8t@!32dq&-7pivlx^aa+t&(7-jq8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1"
+]
 
 
 # Application definition
